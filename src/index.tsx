1f95b0fa7043e8a1d574371a4972f70d6dcc9bec
DHN Chandan <php.chandan@gmail.com>
2022-05-18 01:20:52 +0600

    refactor: :recycle: Remove deprecated ReactChild

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import React, { FC, HTMLAttributes, ReactNode } from "react";
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
+	children?: ReactNode;
 }
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+	return <div>{children || `Sample text`}</div>;
 };
