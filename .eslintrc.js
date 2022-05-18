09d01bf0cf906d100ded99d33eee29cdf2d51374
DHN Chandan <php.chandan@gmail.com>
2022-05-18 23:42:57 +0600

    chore: :art: Implement rush linting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+require("@rushstack/eslint-config/patch/modern-module-resolution");
+
+module.exports = {
+	extends: [
+		"@rushstack/eslint-config/profile/web-app",
+		"@rushstack/eslint-config/mixins/react",
+	],
+	parserOptions: { tsconfigRootDir: __dirname },
+	settings: {
+		react: {
+			version: "17.9",
+		},
+	},
+	rules: {
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				selector: "variable",
+				types: ["boolean"],
+				format: ["PascalCase"],
+				prefix: ["is", "should", "has", "can", "did", "will"],
+			},
+			{
+				selector: "function",
+				format: ["PascalCase"],
+			},
+			{
+				selector: "typeLike",
+				format: ["PascalCase"],
+			},
+		],
+	},
+};
